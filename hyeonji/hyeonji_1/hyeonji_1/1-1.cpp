ed4e2fd4e514f003fcf094b45a5e152502dc445b
PRAHE <wendydeer1020@gmail.com>
2024-01-15 17:17:31 +0900

    Refactor: "Fix by using swtich-case"

--- hyeonji/hyeonji_1/hyeonji_1/1-1.cpp
+++ hyeonji/hyeonji_1/hyeonji_1/1-1.cpp
@@ -1,38 +1,49 @@
 #include <iostream>
 using namespace std;
 
+void triangle(int column, int col);
+
 void main() {
 	int N;
 	cin >> N;
-	for (int col = N-1; col >= 0; col--) {
-		if (col % 3 == 2) {
-			for (int i = 0; i < col; i++)
-				cout << " ";
-			cout << "*";
-			for (int i = 0; i < col; i++)
-				cout << " ";
-			cout << endl;
-		}
-		else if (col % 3 == 1) {
-				for (int i = 0; i < col; i++)
-					cout << " ";
-				cout << "* *";
-				for (int i = 0; i < col; i++)
-					cout << " ";
-				cout << endl;
 
-		}
-		else if (col % 3 == 0) {
-				for (int i = 0; i < col; i++)
-					cout << " ";
-				cout << "*****";
-				for (int i = 0; i < col; i++)
-					cout << " ";
-				cout << endl;
-
-		}
+	for (int col = N-1; col >= 0; col--) 
+	{
+		int column = col%3;
+		triangle(column, col);
 		cout << endl;
 	}
+}
 
+void triangle(int column, int col) {
+	switch (column)
+	{
+	case 2:
+		for (int i = 0; i < col; i++)
+			cout << " ";
+		cout << "*";
+		for (int i = 0; i < col; i++)
+			cout << " ";
+		cout << endl;
+		break;
+
+	case 1:
+		for (int i = 0; i < col; i++)
+			cout << " ";
+		cout << "* *";
+		for (int i = 0; i < col; i++)
+			cout << " ";
+		cout << endl;
+		break;
+
+	case 0:
+		for (int i = 0; i < col; i++)
+			cout << " ";
+		cout << "*****";
+		for (int i = 0; i < col; i++)
+			cout << " ";
+		cout << endl;
+		break;
+	}
 
 }
