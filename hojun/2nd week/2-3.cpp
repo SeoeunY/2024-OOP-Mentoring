476369088814056167286e937170a9bd33f2bff3
sousky <hojun3338@naver.com>
2024-01-22 20:27:32 +0900

    Feat: add 4th code of 2nd week & comment: 3rd code of 2nd week

--- hojun/2nd week/2-3.cpp
+++ hojun/2nd week/2-3.cpp
@@ -1,45 +1,48 @@
 #include <iostream>
 using namespace std;
 
+//////func declare
 void merge_sort(int st, int en);
 void merge(int st, int en);
 
+///input arr & temp arr
 int a[100000];
 int tmp[100000];
 
 int main(void)
 {
-    int n;
+    int n;//the number of inputs in advance.
     cin >> n;
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-    merge_sort(0, n);
 
-    for (int i = 0; i < n; i++) 
-        cout << a[i] << ' ';
+    for (int i = 0; i < n; i++)//number input loop
+        cin >> a[i];
+
+    merge_sort(0, n);//sort func call
+
+    cout << a[(n - 1) / 2];//output print
     return 0;
 }
 
 void merge_sort(int st, int en)
 {
-    if (en == (st + 1)) return;
-    int mid = (st + en) / 2;
-    merge_sort(st, mid);
-    merge_sort(mid, en);
-    merge(st, en);
+    if (en == (st + 1)) return;//base condition
+    int mid = (st + en) / 2;//mid reset
+    merge_sort(st, mid);//recursive call
+    merge_sort(mid, en);//recursive call
+    merge(st, en);//merge func call
 }
 
 void merge(int st, int en)
 {
-    int mid = (st + en) / 2;
-    int idx1 = st;
-    int idx2 = mid;
+    int mid = (st + en) / 2;//mid reset
+    int idx1 = st;//start point reset
+    int idx2 = mid;//end point reset
     for (int i = st; i < en; i++) {
-        if (idx2 == en) tmp[i] = a[idx1++];
-        else if (idx1 == mid) tmp[i] = a[idx2++];
-        else if (a[idx1] <= a[idx2]) tmp[i] = a[idx1++];
-        else tmp[i] = a[idx2++];
+        if (idx2 == en) tmp[i] = a[idx1++];//idx2 val end
+        else if (idx1 == mid) tmp[i] = a[idx2++];//idx1 val end
+        else if (a[idx1] <= a[idx2]) tmp[i] = a[idx1++];//value compare
+        else tmp[i] = a[idx2++]; //value compare
     }
-    for (int i = st; i < en; i++)
+    for (int i = st; i < en; i++)//move to std arr from the temp arr
         a[i] = tmp[i];
 }
---
+++ hojun/2nd week/2-4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a[100000];//input arr
+
+
+void quick_sort(int st, int en);//func declare
+
+int main(void)
+{
+	int n;//the number of inputs in advance.
+	cin >> n;
+	for (int i = 0; i < n; i++)//number input loop
+		cin >> a[i];
+
+	quick_sort(0, n);//sort func call
+	cout << a[(n - 1) / 2];//output print
+	return 0;
+}
+
+void quick_sort(int st, int en)
+{
+	if (en <= st + 1)//base condition
+		return;
+
+	int pivot = a[st];//pivot reset
+	int id1 = st + 1;//index reset
+	int id2 = en - 1;//index reset
+
+	while (1)
+	{
+		while (id1 <= id2 && a[id1] <= pivot) id1++;//index move
+		while (id1 <= id2 && a[id2] > pivot) id2--;//index move
+		if (id1 > id2) break;//loop break if
+		swap(a[id1], a[id2]);//swap afte value compare
+	}
+	swap(a[st], a[id2]);//pivot and smaller than pivot value swap
+	quick_sort(st, id2);//recursive call
+	quick_sort(id2 + 1, en);//recursive call
+}
