6430821cd1a41f75b670308e60c135139b6f4cf9
sousky <hojun3338@naver.com>
2024-01-31 15:03:39 +0900

    fix: 1st code of 3rd week

--- hojun/3rd week/3-1.cpp
+++ hojun/3rd week/3-1.cpp
@@ -6,24 +6,24 @@ using namespace std;
 class Employee//class declare
 {
 private:
-	char* name;
+	char name[20];
 	int age = 0;
-	char* country;
-	char* job;
+	char country[20];
+	char job[20];
 public:
 	Employee(char* name, int age, char* country, char* job);
 	bool isNameCorrect(char* name) const;
-	void print();
+	void print() const;
 	void change(char* name, int age, char* country, char* job);
 };
 
-Employee* emp[10];
+Employee* emp[10];//employee array
 
 int main()
 {
 	int emp_num = 0;//employee number
 	char input[10] = {};//command input array
-
+	char fi[20];
 	///input information variable
 	char name[20];
 	int age;
@@ -38,26 +38,40 @@ int main()
 		if (strcmp(input, "exit") == 0) { break; }
 		else if (strcmp(input, "print") == 0)
 		{
+			cout << "=====print=====\n";
 			for (int i = 0; i < emp_num; i++)
 				emp[i]->print();
 		}
 		else if (strcmp(input, "insert") == 0)
 		{
+			cout << "새로운 직원\n";
 			cin >> name >> age >> country >> job;
-			emp[emp_num++] = new Employee(name, age, country, job);
+			emp[emp_num] = new Employee(name, age, country, job);
+			emp_num++;
 		}
 		else if (strcmp(input, "find") == 0)
 		{
-			cin >> input;
+			cin >> fi;
 			for (int i = 0; i < emp_num; i++)
 			{
-				if (emp[i]->isNameCorrect(input))
+				if (emp[i]->isNameCorrect(fi))
+				{
+					cout << "=====find=====\n";
 					emp[i]->print();
+				}
 			}
 		}
 		else if (strcmp(input, "change") == 0)
 		{
-
+			cin >> input >> name >> age >> country >> job;
+			for (int i = 0; i < emp_num; i++)
+			{
+				if (emp[i]->isNameCorrect(input))
+				{
+					emp[i]->change(name, age, country, job);
+					break;
+				}
+			}
 		}
 
 	}
@@ -66,38 +80,31 @@ int main()
 //////class member function define part/////////
 Employee::Employee(char* name, int age, char* country, char* job)
 {
-	this->name = name;
+	strcpy(this->name, name);
+	strcpy(this->country, country);
+	strcpy(this->job, job);
 	this->age = age;
-	this->country = country;
-	this->job = job;
 }
 
 bool Employee::isNameCorrect(char* name) const
 {
-	if (strcmp(this->name, name) == 0)
-		return true;
-	else
-		return false;
+	bool k = !strcmp(this->name, name);
+	return k;
 }
 
-void Employee::print()
+void Employee::print() const
 {
-	cout << "=====print=====\n";
-	for (int i = 0; i < 10; i++)
-	{
-		if (emp[i]->age != 0)
-		{
-			cout << "Name: " << emp[i]->name;
-			cout << "Age: " << emp[i]->age;
-			cout << "Country: " << emp[i]->country;
-			cout << "Job: " << emp[i]->job;
-		}
-		else
-			break;
-	}
+	cout << "Name: " << this->name << endl;
+	cout << "Age: " << this->age << endl;
+	cout << "Country: " << this->country << endl;
+	cout << "Job: " << this->job << endl;
+	cout << "----------\n";
 }
 
 void Employee::change(char* name, int age, char* country, char* job)
 {
-	return;
+	strcpy(this->name, name);
+	strcpy(this->country, country);
+	strcpy(this->job, job);
+	this->age = age;
 }
